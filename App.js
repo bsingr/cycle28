dd6f8c8697f3eae1aae8074d3fac5cd38281d02d
Jens Bissinger <mail@jens-bissinger.de>
2019-10-25 17:40:29 +0200

    Add authentication with face, touch or password

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
+  Alert,
 } from 'react-native';
 
 import {
@@ -30,8 +32,10 @@ import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 import {read, write} from './src/storage'
 import CalendarModel from './src/calendarModel'
+import TouchID from 'react-native-touch-id';
 
 const App: () => React$Node = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [calendar, setCalendar] = useState(new CalendarModel());
   const [menstruationDays, setMenstruationDays] = useState({});
   const [menstruationStats, setMenstruationStats] = useState(null)
@@ -39,7 +43,7 @@ const App: () => React$Node = () => {
 
   // load all data once initially
   useEffect(() => {
-    if (!isLoaded) {
+    if (isAuthenticated && !isLoaded) {
       read().then(data => {
         calendar.load(data);
         setMenstruationDays(Object.assign({}, calendar.getMenstruationDays()));
@@ -47,7 +51,7 @@ const App: () => React$Node = () => {
         setIsLoaded(true);
       })
     }
-  }, [isLoaded]);
+  }, [isAuthenticated, isLoaded]);
 
   // persist data after (but skip if it was never loaded before)
   useEffect(() => {
@@ -64,42 +68,64 @@ const App: () => React$Node = () => {
     headerText = `Last period was ${menstruationStats.whenWasLastInterval} days ago lasting ${menstruationStats.lengthOfLastInterval} days
 Usually a period happens every ${menstruationStats.p90Interval} days lasting ${menstruationStats.p90Length} days`
   }
+
+
+  if (!isAuthenticated) {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.authenticateContainer}>
+          <View style={styles.authenticate}>
+            <Button
+              title="Authenticate Cycle28"
+              onPress={() => {
+              TouchID.authenticate('Authenticate to unlock sensible data', {passcodeFallback: true})
+                .then(success => {
+                  setIsAuthenticated(true)
+                })
+                .catch(error => {
+                  Alert.alert('Authentication Failed. Try Again!');
+                });
+            }} />
+          </View>
+        </SafeAreaView>
+      </>
+    )
+  }
   
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.main}>
-          <View style={styles.header}>
-            <Text style={styles.headerText}>
-              {headerText}
-            </Text>
-          </View>
-          <View style={styles.calendar}>
-            <CalendarList
-              firstDay={1}
-              onDayPress={day => {
-                if (menstruationDays[day.dateString]) {
-                  calendar.remove(day.dateString);
-                } else {
-                  calendar.add(day.dateString);
-                }
-                setMenstruationDays(Object.assign({}, calendar.getMenstruationDays()))
-              }}
-              // Max amount of months allowed to scroll to the past. Default = 50
-              pastScrollRange={24}
-              // Max amount of months allowed to scroll to the future. Default = 50
-              futureScrollRange={1}
-              // Enable or disable scrolling of calendar list
-              scrollEnabled={true}
-              // Enable or disable vertical scroll indicator. Default = false
-              showScrollIndicator={true}
-              current={calendar.getToday()}
-              markedDates={menstruationDays}
-              // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
-              markingType={'period'}
-            />
-          </View>
+      <SafeAreaView style={styles.main}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>
+            {headerText}
+          </Text>
+        </View>
+        <View style={styles.calendar}>
+          <CalendarList
+            firstDay={1}
+            onDayPress={day => {
+              if (menstruationDays[day.dateString]) {
+                calendar.remove(day.dateString);
+              } else {
+                calendar.add(day.dateString);
+              }
+              setMenstruationDays(Object.assign({}, calendar.getMenstruationDays()))
+            }}
+            // Max amount of months allowed to scroll to the past. Default = 50
+            pastScrollRange={24}
+            // Max amount of months allowed to scroll to the future. Default = 50
+            futureScrollRange={1}
+            // Enable or disable scrolling of calendar list
+            scrollEnabled={true}
+            // Enable or disable vertical scroll indicator. Default = false
+            showScrollIndicator={true}
+            current={calendar.getToday()}
+            markedDates={menstruationDays}
+            // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
+            markingType={'period'}
+          />
         </View>
       </SafeAreaView>
     </>
@@ -107,7 +133,18 @@ Usually a period happens every ${menstruationStats.p90Interval} days lasting ${m
 };
 
 const styles = StyleSheet.create({
+  authenticateContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  authenticate: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   main: {
+    flex: 1,
   },
   calendar: {
     backgroundColor: Colors.white,
